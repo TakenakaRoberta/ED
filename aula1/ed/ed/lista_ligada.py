85550489bf6339ba60e0f1d2006facd72f9ffa29
Roberta Takenaka <takenaka.roberta@gmail.com>
2019-09-18 23:43:18 -0300

    Aula 1 - Video 1.2 - Lista Ligada Introdução

---
+++ aula1/ed/ed/lista_ligada.py
@@ -0,0 +1,25 @@
+class Celula:
+
+    def __init__(self, conteudo):
+        self.conteudo = conteudo
+        self.proximo = None
+
+class ListaLigada:
+
+    def __init__(self):
+        self._inicio = None
+        self._quantidade = 0
+
+    @property
+    def inicio(self):
+        return self._inicio
+
+    @property
+    def quantidade(self):
+        return self._quantidade
+
+    def imprimir(self):
+        celula_atual = self.inicio
+        for i in range(0, self.quantidade):
+            print(celula_atual.conteudo)
+            celula_atual = celula_atual.proximo
