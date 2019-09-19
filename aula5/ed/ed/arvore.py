0740518cfc8297df48e4bdd60f658991c5bf179e
Roberta Takenaka <takenaka.roberta@gmail.com>
2019-09-19 07:04:37 -0300

    Aula 5 - Video 5.1

---
+++ aula5/ed/ed/arvore.py
@@ -0,0 +1,38 @@
+from ed.lista_duplamente_ligada import ListaDuplamenteLigada
+
+
+class ListaDeNodos(ListaDuplamenteLigada):
+
+    def __init__(self):
+        super().__init__()
+
+    def imprimir(self, nivel):
+        atual = self.inicio
+        for i in range(0, self.quantidade):
+            atual.conteudo.imprimir(nivel)
+            atual = atual.proximo
+
+
+class Nodo:
+
+    def __init__(self, conteudo):
+        self.conteudo = conteudo
+        self.pai = None
+        self.filhos = None
+
+    def __repr__(self):
+        return self.conteudo
+
+    def imprimir(self, nivel=0):
+        print("{}- {}".format(" "*nivel, self.conteudo))
+        if self.filhos:
+            self.filhos.imprimir(nivel+1)
+
+
+class Arvore:
+
+    def __init__(self, conteudo):
+        self.raiz = Nodo(conteudo)
+
+    def imprimir(self):
+        self.raiz.imprimir()
---
+++ aula5/ed/menu.py
@@ -0,0 +1,10 @@
+from ed.arvore import Arvore
+
+
+def main():
+
+    livraria = Arvore("Livros")
+    livraria.imprimir()
+
+
+main()
